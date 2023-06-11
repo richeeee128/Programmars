a0ace22c7190ff77e283b274d70cd6989d73a365
Leeyeon <qoqw808@gmail.com>
2023-06-11 19:53:51 +0900

    두 수의 나눗셈

--- 03.js
+++ 03.js
@@ -1,3 +1,4 @@
 const solution = (num1, num2) => {
   return Math.floor(num1 / num2);
 };
+//Math.floor 는 소수점을 내림한다.
---
+++ 04.js
@@ -0,0 +1,4 @@
+const solution = (num1, num2) => {
+  return Math.trunc((num1 / num2) * 1000);
+};
+//Math.trunc 는 소수점 이하 모두 버림 메서드다.
